7882a792fa63bc6de8a6ab4b517c17cde21db46e
yinasaurus <shiyin.li.2024@smu.edu.sg>
2025-10-21 07:01:30 +0000

    updated ex3

--- src/components/Ex3.vue
+++ src/components/Ex3.vue
@@ -1,9 +1,52 @@
 <script>
+import axios from 'axios';
     export default { 
 
        // add code here
+       data(){
+        return{
+            moods:["happy","sad","angry"],
+            subject:"",
+            entry:"",
+            selectedMood:"",
+            outputMsg:""
+        }
+       },
+       computed: {
+        baseUrl() {
+            if (window.location.hostname=='localhost')
+                return 'http://localhost:3000' 
+            else {
+                const codespace_host = window.location.hostname.replace('5173', '3000')
+                return `https://${codespace_host}`;
+            }
+        }
+    },
+    methods:{
+        addPost(){
+            axios.get(`${this.baseUrl}/addPost`,{
+                params: 
+                {"entry":this.entry,
+                "subject":this.subject,
+                "mood":this.selectedMood,
+            }
+            }).then(response => {
+                console.log('Post added successfully', response);
+                // Clear form after successful submission
+                this.subject = "";
+        this.entry = "";
+        this.selectedMood = "";
+              
+               outputMsg=response.data.message;
+            
+            })
+            .catch(error => {
+                console.error('Error adding post:', error);
+            });
+    }
 
     }
+}
 </script>
 
 <template>
@@ -19,14 +62,18 @@
 
         Mood:
         <!-- TODO: Build a dropdown list here for selecting the mood -->
+         <select v-model="selectedMood">
+            <option v-for="mood in moods">{{ mood }}</option>
+         </select>
+
 
         <br>
-
+         <p v-if="outputMsg">{{ outputMsg }}</p>
         <br>
-        <button>Submit New Post</button>
+        <button @click="addPost()">Submit New Post</button>
 
         <hr> Click  <a><router-link to="/ViewPosts/">here</router-link></a>  to return to Main Page
-       
+        
     </div>
 </template>
 
