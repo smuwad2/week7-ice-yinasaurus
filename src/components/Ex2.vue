657811f02863f5c86a845f97a2714c9c534e1881
yinasaurus <shiyin.li.2024@smu.edu.sg>
2025-10-21 05:48:01 +0000

    updated ex2

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -1,18 +1,41 @@
 <script>
+
     export default { 
         // Add code in appropriate places
         data() {
             return {
-                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter']
+                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'],
+                addItem:""
             }
-            
-        }
+        },
+            methods:{
+                deleteItem(index){
+                    this.items.splice(index,1);
+
+                },
+                addToList(){
+                    if (this.addItem.trim()!=""){
+                        this.items.push(this.addItem.trim())
+                    }
+                    this.addItem="";
+
+                }
+            }
     }
+
 </script>
 
 <template>
 
     <h2>Shopping Cart</h2>
     <!-- TODO: Add Code Here -->
+     <ul>
+        <li v-for="(item,i) in items">
+            {{ item }}
+             <button @click="deleteItem(i)">Delete</button>
+        </li>
+     </ul>
+     <input type="text" v-model="addItem"></input>
+     <button @click="addToList">add!</button>
    
 </template>
